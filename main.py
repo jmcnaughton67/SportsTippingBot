d140c5ebfae65fad3e2fa509a29a38578c77fa6a
jmcnaughton67 <45418369+jmcnaughton67@users.noreply.github.com>
2023-03-07 12:33:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -8,46 +8,78 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 
 def getFixtures(round):
+    # init game count
+    game_count = 0
+
+    # Get Fixture Data
     url = 'https://www.nrl.com/draw//data'
     params = {'competition': '111', 'round': str(round), 'season': '2023'}
-
-
     response = requests.get(url, params=params)
     data = response.json()
-
-    game_count = 0
     fixtures = []
+
+    # Get Ladder Position data
+    ladder_url = 'https://www.nrl.com/ladder/data'
+    ladder_params = {'competition': '111', 'Seasons': '2023'}
+    ladder_response = requests.get(ladder_url, params=ladder_params)
+    ladder_data = ladder_response.json()
+    ladder = []
+    for team in ladder_data['positions']:
+        ladder_team = team['teamNickname']
+        ladder.append(ladder_team)
+
+    # Set the fixture information for each game of the round
     for fixture in data['fixtures']:
+        # Get Game information
         game_count = game_count + 1
         home_team = fixture['homeTeam']['nickName']
-        home_odds = fixture['homeTeam']['odds']
-        home_pos = fixture['homeTeam']['teamPosition']
         home_image = 'https://www.nrl.com/.theme/' + home_team.replace(" ", "-").lower() + '/badge.svg?bust=202302142313'
-
         away_team = fixture['awayTeam']['nickName']
-        away_odds = fixture['awayTeam']['odds']
-        away_pos = fixture['awayTeam']['teamPosition']
-        venue = fixture['venue']
         away_image = 'https://www.nrl.com/.theme/' + away_team.replace(" ", "-").lower() + '/badge.svg?bust=202302142313'
-        print(fixture['clock']['kickOffTimeLong'])
+        venue = fixture['venue']
+        match_status = fixture['matchState']
+        home_pos = ladder.index(home_team) + 1
+        away_pos = ladder.index(away_team) + 1
+
+        #If the game has not started, scores do not exist
+        try:
+            home_score = fixture['homeTeam']['score']
+            away_score = fixture['awayTeam']['score']
+        except:
+            home_score = "0"
+            away_score = "0"
+
+        # If the game has already started - odds are no longer available, so set this to N/A in this case:
+        # This will be fixed
+        if match_status == "Upcoming":
+            home_odds = fixture['homeTeam']['odds']
+            away_odds = fixture['awayTeam']['odds']
+        else:
+            home_odds = "N/A"
+            away_odds = "N/A"
+
         # parse the datetime string into a datetime object
         dt = datetime.strptime(fixture['clock']['kickOffTimeLong'], '%Y-%m-%dT%H:%M:%SZ')
 
         # extract the date and time components into separate variables
         date_component = dt.date()
-        date =  date_component.strftime('%A, %d' + ('th' if 11<=date_component.day<=13 else {1:'st', 2:'nd', 3:'rd'}.get(date_component.day%10, 'th')) + ' %B')
+        date = date_component.strftime('%A, %d' + ('th' if 11<=date_component.day<=13 else {1:'st', 2:'nd', 3:'rd'}.get(date_component.day%10, 'th')) + ' %B')
         time = dt - timedelta(hours=13)
         time = time.strftime('%I:%M %p')
 
         #Add calculation based on odds and ladder position. Return True for the most important game.
-        isMainGame = False
+        # just using game 2 for testing until the ladder position issue is resolved
+        if game_count == 2:
+            isMainGame = True
+        else:
+            isMainGame = False
 
-        fixtures.append({'Round': round, 'Home Team': home_team, 'Home Odds': home_odds, 'Home Position': home_pos, 'Home Image': home_image, 'Away Team': away_team, 'Away Odds': away_odds, 'Away Position': away_pos,  'Away Image': away_image,'Venue': venue, 'Date': date, 'Time': time, 'MainGame': isMainGame})
+        fixtures.append({'Round': round, 'Home Team': home_team, 'Home Odds': home_odds, 'Home Position': home_pos, 'Home Image': home_image, 'Home Score': home_score, 'Away Team': away_team, 'Away Odds': away_odds, 'Away Position': away_pos,  'Away Image': away_image, 'Away Score': away_score, 'Venue': venue, 'Date': date, 'Time': time, 'MainGame': isMainGame, 'Match Status': match_status})
 
     df = pd.DataFrame(fixtures)
     df.index += 1
-    #sort df here by MainGame, so that the game with True is at the top.
-    #This will mean it'll show top on the webpage.
+    #sort df here by MainGame, so that the game with True is at the top so it appears top
+    # We don't want mainGame top of the webpage - keep it in Chronological order - JM
 
     return df
 
@@ -89,7 +121,7 @@ def saveToSheet(name, code, tips, margins):
         tip_sheet['D'+str(cell)].value = margins[i]
 
     wb.save(filepath)
-    print("saved to " +filepath)
+    print("saved to " + filepath)
 
 
 def getFromSheet(code):
@@ -139,7 +171,7 @@ def submit():
     tips = []
     margins = []
 
-    for i in range(1,int(games)+1):
+    for i in range(1, int(games)+1):
         tips.append(request.form['game-'+str(i)])
         margins.append(request.form['game-'+str(i)+"-margin"])
 
@@ -158,7 +190,6 @@ def newCode():
     createCode(code)
 
     return render_template('index.html', generated ="code generated:"+code)
-
 
 
 if __name__ == '__main__':
